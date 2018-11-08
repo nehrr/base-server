34ca9da10b7b792b8f79735e34dd81814ee745b1
ERNOUL <iamkapitaan@gmail.com>
2018-11-08 16:03:44 +0100

    mailer and token

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import passport from 'passport';
 import { mLog } from './lib/utils';
 import { db as database } from './models';
 import routes from './routes';
+import './middleware/passport';
 
 dotenv.config();
 const port = parseInt(process.argv[2], 10) || process.env.PORT;
 
 const app = express();
 
-(async () => {
+app.use(passport.initialize());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const start = async () => {
   try {
     await database.authenticate();
     if (process.env.APP === 'development') {
-      database.sync({ force: process.env.DATABASE_SYNC_FORCE });
+      database.sync({ force: false });
     }
 
     app.use('/api', routes);
@@ -28,4 +35,6 @@ const app = express();
   } catch (e) {
     throw e;
   }
-})();
+};
+
+start();
